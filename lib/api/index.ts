417a1f0dab33f26556c34365b3b9226d4ae2a967
zulfikar <storyciks@gmail.com>
2025-05-23 05:03:02 +0700

    fux: import error fixed for build eror production

--- lib/api/index.ts
+++ lib/api/index.ts
@@ -1,13 +1,26 @@
+// Types
 export * from './types/common'
 export * from './types/auth'
 export * from './types/category'
 export * from './types/item'
 export * from './types/customer'
 export * from './types/rental'
-export * from './services/auth.service'
-export * from './services/category.service'
-export * from './services/item.service'
-export * from './services/customer.service'
-export * from './services/rental.service'
+
+// Services
+import { authService } from './services/auth.service'
+import { categoryService } from './services/category.service'
+import { itemService } from './services/item.service'
+import { customerService } from './services/customer.service'
+import { rentalService } from './services/rental.service'
+
+export {
+  authService,
+  categoryService,
+  itemService,
+  customerService,
+  rentalService
+}
+
+// Utils and Config
 export * from './utils/api-request'
 export * from './config' 
